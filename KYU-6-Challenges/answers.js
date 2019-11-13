cc9a5e1bd252d29445dfec95ccf2268b63af98fe
Alexander Rowland <arowland900@gmail.com>
2019-11-13 15:33:57 -0800

    add first solution for kyu 5 challenge 1

--- KYU-6-Challenges/answers.js
+++ KYU-6-Challenges/answers.js
@@ -1,8 +1,15 @@
 // Challenge 1
 
+// Solution 1 -- this solution works well, but we can also solve this problem more concisely
 function redarr(arr) {
     let res = {}
     arr = [...new Set(arr)].sort()
     for (let i in arr) res[i] = arr[i]
     return res
+}
+
+// Solution 2 -- this seems like the most concise way to go about writing this algorithm
+
+function redarr(arr) {
+    return Object.assign({}, [...new Set(arr)].sort())
 }
