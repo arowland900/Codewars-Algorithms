5bbccc373c60efbed9adeb986adc22f888f6c5b3
Alexander Rowland <arowland900@gmail.com>
2019-12-13 14:21:25 -0800

    add solution 1 for kyu 5 challenge 2

--- KYU-5-Challenges/answers.js
+++ KYU-5-Challenges/answers.js
@@ -56,4 +56,31 @@ function buddy(start, limit) {
 
 /* ---------- CHALLENGE 2 (PICK PEAKS) ---------- */
 
+// Solution 1
 
+function pickPeaks(arr) {
+
+    var obj = {}
+    obj.pos = []
+    obj.peaks = []
+
+    arr.forEach((e, i) => {
+        if (i > 0 && i < arr.length - 2) {
+            if (e > arr[i - 1] && e > arr[i + 1]) {
+                obj.pos.push(i)
+                obj.peaks.push(e)
+            }
+            else if ((e > arr[i - 1] && e == arr[i + 1])) {
+                var j = 2
+                while (arr[i + 1] == arr[i + j]) {
+                    j++
+                }
+                if (arr[i + 1] > arr[i + j]) {
+                    obj.pos.push(i)
+                    obj.peaks.push(e)
+                }
+            }
+        }
+    })
+    return obj
+}
